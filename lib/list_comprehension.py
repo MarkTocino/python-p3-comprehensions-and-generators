a6eb59783e73a38649cc3d6764d90c4ff3642ca0
MarkTocino <Mark_tocino@yahoo.com>
2023-07-24 17:29:00 -0400

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    if not even_numbers:
+        return[]
+    return even_numbers 
     pass
 
 def make_exclamation(sentence_list):
+    list_of_sentences = [sentences + '!' for sentences in sentence_list]
+    if not list_of_sentences:
+        return []
+    return list_of_sentences
     pass
